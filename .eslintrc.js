c5772b5f9ba91db0df705ea07236b2dc79fcef91
ValberJunior <valberjr1992@gmail.com>
2025-06-27 18:45:03 -0300

    chore: update ESLint configuration for TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 };
